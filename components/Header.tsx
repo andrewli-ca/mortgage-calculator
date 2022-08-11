d48f97e8a6ef0259b832a5d482dbaa2ca968da53
Andrew Li <mail@andrewli.ca>
2022-08-10 23:15:35 -0700

    header component

---
+++ components/Header.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import styled from 'styled-components';
+import Hamburger from '../public/hamburger.svg';
+import Logo from '../public/p8Wordmark_sm.svg';
+import { Container } from './sharedstyles';
+
+const StyledHeader = styled.header`
+  background-color: #fff;
+  height: 52px;
+  position: relative;
+  z-index: 1;
+`;
+
+const StyledNav = styled.nav`
+  padding: 12px 16px;
+  height: 100%;
+`;
+
+const HeaderContainer = styled(Container)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1280px;
+  margin: auto;
+`;
+
+function Header() {
+  return (
+    <StyledHeader>
+      <StyledNav>
+        <HeaderContainer>
+          <Image src={Logo} alt="Logo" />
+          <Image src={Hamburger} alt="Menu" />
+        </HeaderContainer>
+      </StyledNav>
+    </StyledHeader>
+  );
+}
+
+export { Header };
