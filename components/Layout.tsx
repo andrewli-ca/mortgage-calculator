c17e1b9c5c3564826884b9fdac57f85411268663
Andrew Li <mail@andrewli.ca>
2022-08-11 22:58:55 -0700

    fix hydration errors

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Fragment, ReactNode } from 'react';
+import React, { Fragment, ReactNode } from 'react';
 import styled from 'styled-components';
 import { Header } from './Header';
 
@@ -8,6 +8,15 @@ const Main = styled.main`
 `;
 
 export default function Layout({ children }: { children: ReactNode }) {
+  const [hasMounted, setHasMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setHasMounted(true);
+  }, []);
+  if (!hasMounted) {
+    return null;
+  }
+
   return (
     <Fragment>
       <Header />
